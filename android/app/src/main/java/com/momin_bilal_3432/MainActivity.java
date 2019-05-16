d6cb6d64701bf71364bab605c094fb4cd6c8a29a
Crowdbotics <team@crowdbotics.com>
2019-05-16 09:58:42 +0000

    initial commit

---
+++ android/app/src/main/java/com/momin_bilal_3432/MainActivity.java
@@ -0,0 +1,15 @@
+package com.momin_bilal_3432;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "momin_bilal_3432";
+    }
+}
